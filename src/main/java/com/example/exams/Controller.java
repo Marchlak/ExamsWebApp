8b48437ee0f0d7e038872f264bc7301e1676921b
Mateusz Mogielnicki <82816@student.pb.edu.pl>
2023-11-18 19:17:43 +0100

    Dodano strone login.html oraz odpowiednie do niej style login.css

--- src/main/java/com/example/exams/Controller.java
+++ src/main/java/com/example/exams/Controller.java
@@ -2,6 +2,7 @@ package com.example.exams;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Controller {
@@ -13,5 +14,11 @@ public class Controller {
         return modelAndView;
     }
 
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
 
 }
