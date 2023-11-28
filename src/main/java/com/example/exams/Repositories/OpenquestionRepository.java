a23cc3f26206e3063d25396237979037e798223b
marchlak <bingor1@wp.pl>
2023-11-28 22:27:06 +0100

    Stworzenie funkcjonalności usuwania pytań backend

---
+++ src/main/java/com/example/exams/Repositories/OpenquestionRepository.java
@@ -0,0 +1,7 @@
+package com.example.exams.Repositories;
+
+import com.example.exams.Model.Data.Openquestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OpenquestionRepository extends JpaRepository<Openquestion, Integer> {
+}
