b1a3eeb72e22f2f3d316a69c7c5ab780643cb8aa
Dominik Mierzejewski <82808@student.pb.edu.pl>
2023-12-05 17:02:54 +0100

    Dodano wyswietlanie godziny do daty, zmieniono przekierowywanie podczas tworzenia egzaminu, poprawiono wyświetlanie przyciskow w odpowiedniej kolumnie.

--- src/main/java/com/example/exams/Controllers/ExamController.java
+++ src/main/java/com/example/exams/Controllers/ExamController.java
@@ -6,12 +6,9 @@ import com.example.exams.Model.Data.db.Subject;
 import com.example.exams.Services.EgzaminatorService;
 import com.example.exams.Services.ExamService;
 import com.example.exams.Services.SubjectService;
-import jdk.jfr.Description;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ExamController {
@@ -26,7 +23,7 @@ public class ExamController {
     EgzaminatorService egzaminatorService;
 
     @PostMapping("/addExam/{egzaminator_egzaminator_id}")
-    public ResponseEntity<String> UpdateOpenQuestion(@ModelAttribute Exam exam, @PathVariable Integer egzaminator_egzaminator_id, @RequestParam Integer subjectid){
+    public String UpdateOpenQuestion(@ModelAttribute Exam exam, @PathVariable Integer egzaminator_egzaminator_id, @RequestParam Integer subjectid){
 
         //Testy
         egzaminator_egzaminator_id = 1;
@@ -38,7 +35,7 @@ public class ExamController {
         exam.setSubjectSubjectid(subject);
 
         Exam addedExam = examService.AddExam(exam);
-        return ResponseEntity.ok("Question updated successfully");
+        return "redirect:/exams";
     }
 }
 
