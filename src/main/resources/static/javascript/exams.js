769acd96e608bc5f27f778ae7ddc8f341570126d
mat3usq <mmogielnicki123@gmail.com>
2024-03-16 18:22:22 +0100

    repair cancel button in view createExam

--- src/main/resources/static/javascript/exams.js
+++ src/main/resources/static/javascript/exams.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     let createExamButton = document.querySelector('.create-exam-submit')
-    let editExamButton = document.querySelector('.edit-exam')
     let createExamBox = document.querySelector('.create-exam-box')
     let editExamBox = document.querySelector('.edit-exam-box')
     let cancelCreateButton = document.querySelector('.cancel-create-exam')
@@ -84,10 +83,6 @@ document.addEventListener('DOMContentLoaded', function () {
         overlay.classList.toggle('hidden')
     })
 
-    editExamButton.addEventListener('click', function () {
-        editExamBox.classList.toggle('hidden')
-        overlay.classList.toggle('hidden')
-    })
 
     cancelCreateButton.addEventListener('click', function () {
         createExamBox.classList.add('hidden')
